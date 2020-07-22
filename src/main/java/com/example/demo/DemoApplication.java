8b8f5eaf34f8e23fa5c2fa48a3cdaf01b6657a2f
91740 <pradeep4k21@gmail.com>
2020-07-22 17:42:11 +0530

    Second commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("This is to check another repo");
+		System.out.println("Another change");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
